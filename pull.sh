b63be57624fd1387f3e3f8e3decff0d5ec9e8d55
frickingbird8002 <sakul8002@icloud.com>
2024-10-17 01:10:11 +0200

    fixed up some css and stuff

--- pull.sh
+++ pull.sh
@@ -1,8 +1,7 @@
-echo "bitchass"
 cd ~/httpserver
-sudo systemctl stop lukasweb.service
+systemctl stop lukasweb.service
 rm pull.sh
 git pull
 chmod +x pull.sh
 go build main.go
-sudo systemctl start lukasweb.service
+systemctl start lukasweb.service
