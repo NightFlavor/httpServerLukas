6a65df03de0ffff0dd42f0b53468020395e226f3
frickingbird8002 <sakul8002@icloud.com>
2024-10-17 23:43:04 +0200

    hmm

--- main.go
+++ main.go
@@ -9,17 +9,27 @@ import (
 )
 
 func give_website(w http.ResponseWriter, r *http.Request) {
-	htmlFile, _ := os.Open("html/index.html")
+	htmlFile, err := os.Open("html/index.html")
+	if err != nil {
+		http.Error(w, "Could not open file", http.StatusInternalServerError)
+		return
+	}
 	defer htmlFile.Close()
 	w.Header().Set("Content-Type", "text/html")
 	io.Copy(w, htmlFile)
 }
+
 func pull_and_restart(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("pulling and rebooting")
-	cmd := exec.Command("pull.sh")
-	output, _ := cmd.Output()
+	cmd := exec.Command("~httpserver/pull.sh")
+	output, err := cmd.Output()
+	if err != nil {
+		http.Error(w, "Failed to run script", http.StatusInternalServerError)
+		return
+	}
 	fmt.Println(string(output))
 }
+
 func main() {
 	http.HandleFunc("/", give_website)
 	fileServer := http.FileServer(http.Dir("./html"))
